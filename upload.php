9e3dbf72ac24403d72c691ba7cac431b753467ab
lake-contribution <57761186+lake-contribution@users.noreply.github.com>
2020-01-14 16:37:37 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -2,10 +2,10 @@
 
 file_uploads = On
  if (isset($_POST['submit'])) {
-   echo "<p>" . $_POST['myfile'] . " => file input successfull</p>";
+   echo "<p>" . $_POST['file'] . " => file input successfull</p>";
    $target_dir = "Music ";
-   $file_name = $_FILES['myfile']['name'];
-   $file_tmp = $_FILES['myfile']['tmp_name'];
+   $file_name = $_FILES['file']['name'];
+   $file_tmp = $_FILES['file']['tmp_name'];
 
    if (move_uploaded_file($file_tmp, $target_dir . $file_name)) {
       echo "<h1>File Upload Success</h1>";
